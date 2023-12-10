ca318e478a7f607f67ccb807f4c94fb320b70280
Sanisetty kartheek <kartheek@KartheeettysAir.lan>
2023-12-10 12:36:01 -0500

    Add untracked files

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+
+
+function Footer() {
+    return (
+        <footer className="bottom">
+    
+        <div className="center">
+            All rights reserved &copy; Kartheek sanisetty
+        </div>
+    
+</footer>
+  
+    );
+  }
+  
+  export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,15 @@
+
+function Hero() {
+    return (
+   
+          <div className="hero" >
+            <h1>Personal Budget Tracker</h1>
+            <h2>A app for your daily needs</h2>
+        </div>
+     
+        
+  
+    );
+  }
+  
+  export default Hero;
